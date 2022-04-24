f80a75d50c1c863315b3efeebc12b6a8b53feb58
sj2sj <waterttug@naver.com>
2022-04-24 22:31:51 +0900

    6-3 삽입정렬

---
+++ lecture_algorithm_solv/chapter_6_Sorting and Searching (정렬, 이분검색과 결정알고리즘)/6-3. 삽입정렬.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+
+/* 선택 정렬 */
+
+public class Main {
+	public int[] solution(int n, int[] arr) {
+		
+		for (int i = 1; i < n; i++) {
+			int temp = arr[i];
+			for (int j = i-1; j >= 0; j--) {
+				if (arr[j] > temp) {
+					arr[j+1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt(); //배열 길이
+		
+		int[] arr = new int[n]; 
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		
+		for (int x : T.solution(n, arr)) System.out.print(x + " ");
+	}
+}
+
